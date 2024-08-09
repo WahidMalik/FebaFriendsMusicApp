0a0efc0fd18c0f66e08e98b16f312c03b4d110ba
WahidMalik <malikwahi705@gmail.com>
2024-08-09 12:35:01 +0500

    Change

---
+++ app/src/main/java/com/example/febafriends/SignUp.kt
@@ -0,0 +1,26 @@
+package com.example.febafriends
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.febafriends.databinding.ActivitySignUpBinding
+
+class SignUp : AppCompatActivity() {
+
+    lateinit var binding : ActivitySignUpBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.createAccount.setOnClickListener {
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+            val confirmPassword = binding.confirmPasswordEditText.text.toString()
+        }
+
+    }
+}
