c6ad2452950c854d902586f7cb8397a2fc3c71d8
WahidMalik <malikwahi705@gmail.com>
2024-08-10 18:11:58 +0500

    Change

--- app/src/main/java/com/example/febafriends/ContactusFragment.kt
+++ app/src/main/java/com/example/febafriends/ContactusFragment.kt
@@ -1,27 +1,44 @@
 package com.example.febafriends
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 
 class ContactusFragment : Fragment() {
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    lateinit var num1: TextView
+    lateinit var num2: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contactus, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_contactus, container, false)
+
+        // Initialize the views after inflating the layout
+        num1 = view.findViewById(R.id.num1)
+        num2 = view.findViewById(R.id.num2)
+
+        num1.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL)
+            intent.data = Uri.parse("tel:${num1.text}")
+            startActivity(intent)
+        }
+
+        num2.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL)
+            intent.data = Uri.parse("tel:${num2.text}")
+            startActivity(intent)
+        }
+
+        return view
     }
-
-
 }
--- app/src/main/java/com/example/febafriends/PrivacyPolicyFragment.kt
+++ app/src/main/java/com/example/febafriends/PrivacyPolicyFragment.kt
@@ -1,11 +1,14 @@
 package com.example.febafriends
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.TextView
 
 
 class PrivacyPolicyFragment : Fragment() {
@@ -15,9 +18,19 @@ class PrivacyPolicyFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_privacy_policy, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_contactus, container, false)
+
+        // Initialize the views after inflating the layout
+        gotoUrl()
+
+        return view
     }
 
-
+    private fun gotoUrl() {
+        val url = "https://feba-radio.org/feba-friends-privacy-policy/"
+        val i = Intent(Intent.ACTION_VIEW)
+        i.data = Uri.parse(url)
+        startActivity(i)
+    }
 }
